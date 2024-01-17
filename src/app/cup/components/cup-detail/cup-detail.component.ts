52686cc0b2a658ea0153faa9c0eaa9cc21e1a709
jumpering <jumpering@gmail.com>
2024-01-17 20:38:34 +0100

    wip update, for now less cohesion (together detail and update)

--- src/app/cup/components/cup-detail/cup-detail.component.ts
+++ src/app/cup/components/cup-detail/cup-detail.component.ts
@@ -5,9 +5,11 @@ import { CupService } from '@cup/services/cup.service'
 import { Cup } from '@cup/models/cup.model';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteDialogComponent } from '../cup-delete-dialog/delete-dialog.component';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { BreakpointService } from 'src/app/utils/breakpoint.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from 'src/app/utils/customValidators';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-cup-detail',
@@ -21,13 +23,18 @@ export class CupDetailComponent implements OnInit {
   public updateFields: boolean = false;
   public isHandset$!: Observable<boolean>;
   public file: File | null = null;
+  public form!: FormGroup;
+  public urlImage!: string;
+  private extensionsPermited: string[] = ['jpg', 'jpeg', 'png', 'gif', 'webp'];
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private cupService: CupService,
     private router: Router,
     private matDialog: MatDialog,
-    private breakpointService: BreakpointService
+    private breakpointService: BreakpointService,
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
@@ -63,6 +70,45 @@ export class CupDetailComponent implements OnInit {
 
   onClickUpdateFields(): void {
     this.updateFields = true;
+  }
+
+  onClickSaveFields(): void { //for update cup
+    this.form = this.formBuilder.group({
+      name: ['', //default
+        [Validators.required, Validators.minLength(3)], //sync 
+        [CustomValidators.existCupName(this.cupService)] //async
+      ],
+      origin: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+    })
+  }
+
+  onSelectedFile(event: any): void {
+    this.file =  event.target.files[0];
+    if (this.file && this.isImage()) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        this.urlImage = reader.result as string;
+      };
+      reader.readAsDataURL(this.file);
+    } else {
+      this.file = null;
+      let messageExtensionsPrettyFormat: string = '';
+      this.extensionsPermited.forEach(ext => {
+        messageExtensionsPrettyFormat = messageExtensionsPrettyFormat.concat('*.' + ext + ` `);
+      });
+      this.showSnackBarMessage('Only ' + messageExtensionsPrettyFormat + ' files are supported.');
+    }
+  }
+
+  private isImage(): boolean {
+    const image = this.file?.name.split('.');
+    const extension = image![(image!.length) - 1];
+    return this.extensionsPermited.includes(extension);
+  }
+
+  private showSnackBarMessage(message: string): void {
+    this.snackBar.open(message, '', { duration: 10000 });
   }
 
   updateTitle(): void {
