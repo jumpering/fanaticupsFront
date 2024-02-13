4992e0371686ba46dd395bb67c2cdd57282b2a65
jumpering <jumpering@gmail.com>
2024-02-13 20:17:12 +0100

    add path cupImage to s3 minio nginx reverse proxy

--- src/app/cup/components/cup-detail/cup-detail.component.ts
+++ src/app/cup/components/cup-detail/cup-detail.component.ts
@@ -25,7 +25,8 @@ export class CupDetailComponent implements OnInit {
   //public cupImage: string = 'https://5.250.190.45/images/';
   //public cupImage: string = 'https://images.fanaticups.org/images/';
   //public cupImage: string = 'http://5.250.184.31:8080/images/';
-  public cupImage: string = 'http://5.250.184.31:9000/fanaticups/';
+  //public cupImage: string = 'http://5.250.184.31:9000/fanaticups/';
+  public cupImage: string = 'images/fanaticups/';
 
   public updateFields: boolean = false;
   public isHandset$!: Observable<boolean>;
