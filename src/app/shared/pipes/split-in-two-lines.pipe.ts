f44044294c3c5d2b9d917601361821383252e368
jumpering <jumpering@gmail.com>
2024-04-03 00:20:13 +0200

    refactor style chat

--- src/app/shared/pipes/split-in-two-lines.pipe.ts
+++ src/app/shared/pipes/split-in-two-lines.pipe.ts
@@ -7,6 +7,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SplitInTwoLinesPipe implements PipeTransform {
 
   transform(value: String): String {
+// if(value.length > 50){
+//   // const parts = value.split(' ');
+//   // const middle = Math.ceil(parts.length / 2);
+//   // const firstLine = parts.slice(0, middle).join(' ');
+//   // const secondLine = parts.slice(middle).join(' ');
+//   // return `${firstLine}<br>${secondLine}`;
+// }
+//   return value;
     const parts = value.split(' ');
     const middle = Math.ceil(parts.length / 2);
     const firstLine = parts.slice(0, middle).join(' ');
