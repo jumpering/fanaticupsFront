4f245fc72725ce3c1be5ead013058f3be3927c25
jumpering <jumpering@gmail.com>
2024-09-25 19:16:17 +0200

    update welcome message

--- src/app/cup/components/cup-list/cup-list.component.ts
+++ src/app/cup/components/cup-list/cup-list.component.ts
@@ -1,6 +1,102 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
 
+// import { Cup } from '../../models/cup.model';
+// import { CupService } from '@cup/services/cup.service';
+// import { debounceTime, Subscription } from 'rxjs';
+// import { SearchService } from '@shared/services/search.service';
+// import { Criteria } from '@cup/filterCriteria/criteria';
+// import { CriteriaService } from '@cup/services/criteria.service';
+
+
+// @Component({
+//   selector: 'app-cup-list',
+//   // standalone: true,
+//   // imports: [],
+//   templateUrl: './cup-list.component.html',
+//   styleUrls: ['./cup-list.component.scss'],
+// })
+// export class CupListComponent implements OnInit {
+
+//   public listOfCups: Cup[] = [];
+//   public page: number = 0;
+//   public cupsPerPage: number = 12;
+//   public isFirst: boolean = false;
+//   public isLast: boolean = false;
+//   public searchString: string = '';
+//   public showLoading: boolean = false;
+//   private criteria!: Criteria;
+//   private criteriaSubscription!: Subscription; //for unsubscribe
+
+//   constructor(
+//     private cupService: CupService,
+//     private router: Router,
+//     private searchService: SearchService,
+//     private criteriaService: CriteriaService
+//   ) { }
+
+//   //TODO search aquí?
+//   ngOnInit(): void {
+//     this.criteriaSubscription = this.criteriaService.getCriteria().subscribe(element => {
+//       this.criteria = element;
+//       this.getAllCups(this.criteria);
+//     });
+//     this.searchService.searchTermChanged.pipe(debounceTime(300)).subscribe((searchTerm: string) => {
+//       this.searchString = searchTerm;
+//       this.criteria.cupName = searchTerm;
+//       this.resetPageable();
+//       this.listOfCups = [];
+//       this.getAllCups(this.criteria);
+//     });
+//   }
+
+//   ngOnDestroy(): void {
+//     this.criteriaSubscription.unsubscribe();
+//   }
+
+//   public getAllCups(criteria: Criteria): void {
+//     this.showLoading = true;
+//     this.cupService.getAllCups(this.page, this.cupsPerPage, criteria).subscribe(requestDataInput => {
+//       this.page = requestDataInput.number;
+//       this.isFirst = requestDataInput.first;
+//       this.isLast = requestDataInput.last;
+//       this.showLoading = false;
+//       this.listOfCups.push(...requestDataInput.content);
+//     });
+//   }
+
+//   private resetPageable(): void {
+//     this.page = 0;
+//     this.isFirst = false;
+//     this.isLast = false;
+//   }
+
+//   public onScrollDown() {
+//     this.page++;
+//     if (this.isLast !== true) {
+//       this.getAllCups(this.criteria);
+//     }
+//   }
+
+//   public onBuyClicked(id: number) {
+//     //console.log("buy ID cup: " + id);
+//   }
+
+//   public onDetailClicked(id: number) {
+//     this.router.navigate(['/', id]);
+//   }
+
+//   public onCreateClicked(cup: Cup) {
+//     //this.cupService.
+//   }
+
+// }
+
+
+
+
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
 import { Cup } from '../../models/cup.model';
 import { CupService } from '@cup/services/cup.service';
 import { debounceTime, Subscription } from 'rxjs';
@@ -8,15 +104,12 @@ import { SearchService } from '@shared/services/search.service';
 import { Criteria } from '@cup/filterCriteria/criteria';
 import { CriteriaService } from '@cup/services/criteria.service';
 
-
 @Component({
   selector: 'app-cup-list',
-  // standalone: true,
-  // imports: [],
   templateUrl: './cup-list.component.html',
   styleUrls: ['./cup-list.component.scss'],
 })
-export class CupListComponent implements OnInit {
+export class CupListComponent implements OnInit, AfterViewInit, OnDestroy {
 
   public listOfCups: Cup[] = [];
   public page: number = 0;
@@ -26,7 +119,7 @@ export class CupListComponent implements OnInit {
   public searchString: string = '';
   public showLoading: boolean = false;
   private criteria!: Criteria;
-  private criteriaSubscription!: Subscription; //for unsubscribe
+  private criteriaSubscription!: Subscription; // for unsubscribe
 
   constructor(
     private cupService: CupService,
@@ -35,12 +128,12 @@ export class CupListComponent implements OnInit {
     private criteriaService: CriteriaService
   ) { }
 
-  //TODO search aquí?
   ngOnInit(): void {
     this.criteriaSubscription = this.criteriaService.getCriteria().subscribe(element => {
       this.criteria = element;
       this.getAllCups(this.criteria);
     });
+
     this.searchService.searchTermChanged.pipe(debounceTime(300)).subscribe((searchTerm: string) => {
       this.searchString = searchTerm;
       this.criteria.cupName = searchTerm;
@@ -48,6 +141,10 @@ export class CupListComponent implements OnInit {
       this.listOfCups = [];
       this.getAllCups(this.criteria);
     });
+  }
+
+  ngAfterViewInit(): void {
+    this.adjustCardRows(); // Llama al ajuste de filas después de que se renderice el componente
   }
 
   ngOnDestroy(): void {
@@ -62,6 +159,7 @@ export class CupListComponent implements OnInit {
       this.isLast = requestDataInput.last;
       this.showLoading = false;
       this.listOfCups.push(...requestDataInput.content);
+      setTimeout(() => this.adjustCardRows(), 0); // Ajusta las filas después de cargar más cups
     });
   }
 
@@ -90,4 +188,13 @@ export class CupListComponent implements OnInit {
     //this.cupService.
   }
 
+  private adjustCardRows() {
+    const cards = document.querySelectorAll('.cup-card.isHandset');
+
+    cards.forEach(card => {
+        const cardElement = card as HTMLElement; // Aserción de tipo a HTMLElement
+        const rowSpan = Math.ceil(cardElement.scrollHeight / 10); // Ajusta este divisor según sea necesario
+        cardElement.style.gridRowEnd = `span ${rowSpan}`; // Ahora funciona sin error
+    });
+}
 }
